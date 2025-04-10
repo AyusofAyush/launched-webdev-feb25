7dd688969b1e5c0625755336e9c281bfe89325ba
Ayush Raj <ayusofayush@gmail.com>
2025-04-10 20:40:13 +0530

    Responsive Design & Accessibility

---
+++ Day29_Responsive_Design_WebDev/ecommerce-product-page/src/components/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} My E-Commerce Store</p>
+    </footer>
+  );
+};
+
+export default Footer;
